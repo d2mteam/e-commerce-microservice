ac7e68b7e70b6b39ed63daa348e075e78282cfe6
Lam <ndl091005@gmai.com>
2025-11-05 18:34:30 +0700

    ver2

--- product-service/src/main/java/com/project/productservice/service/ProductService.java
+++ product-service/src/main/java/com/project/productservice/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.project.productservice.service;
 
+import com.project.productservice.entity.Product;
 import com.project.productservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -14,5 +16,9 @@ public class ProductService {
 
     public List<UUID> searchProductIdsByName(String keyword) {
         return productRepository.findProductIdsByNameContaining(keyword);
+    }
+
+    public Optional<Product> getProductByName(String productName) {
+        return productRepository.findByProductName(productName);
     }
 }
