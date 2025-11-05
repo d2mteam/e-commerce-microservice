ff87a800796deee12f78663256033ecac36d7940
Lam <ndl091005@gmail.com>
2025-11-05 17:07:26 +0700

    hehe

--- product-service/src/main/java/com/project/productservice/repository/CategoryRepository.java
+++ product-service/src/main/java/com/project/productservice/repository/CategoryRepository.java
@@ -1,7 +1,10 @@
 package com.project.productservice.repository;
 
 import com.project.productservice.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CategoryRepository extends MongoRepository<Category, Long> {
-}
+//public interface CategoryRepository extends MongoRepository<Category, Long> {
+//}
+public interface CategoryRepository extends JpaRepository<Category, Long> { }
+
