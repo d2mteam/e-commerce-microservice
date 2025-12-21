7edf0a20d941b32fc6987cce16b053fcb8c36f7a
yuk1 <23020110@vnu.edu.vn>
2025-12-22 02:49:37 +0700

    mock payment 2

---
+++ mock-payment/src/test/java/com/example/mockpayment/MockPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
