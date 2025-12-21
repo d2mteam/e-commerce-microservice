dd71946d88c04ac45fc99587275e23bb25080dca
yuk1 <23020110@vnu.edu.vn>
2025-12-22 00:44:17 +0700

    akka

--- order-service/src/main/java/com/project/infrastructure/jpa/repository/InboxEventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.infrastructure.jpa.repository;
-
-import com.project.infrastructure.jpa.entity.InboxEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InboxEventRepository extends JpaRepository<InboxEvent, Long> {
-}
